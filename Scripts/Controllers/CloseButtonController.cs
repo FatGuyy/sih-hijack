1787a4f04ffa3c4696aae953e719f3ad1de8012b
Shivraj-Kore <shivrajkore100@gmail.com>
2023-12-19 12:13:23 +0530

    Add Close Button

---
+++ Scripts/Controllers/CloseButtonController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButtonController : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
